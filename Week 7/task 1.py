d9794484d2778ca7ca110d466affe43cf18006e1
2026-CYS-14 <fizza4112@gmail.com>
2026-05-02 14:20:31 +0500

    Add files via upload

---
+++ Week 7/task 1.py
@@ -0,0 +1,4 @@
+ob_marks = int(input("Enter your marks: "))
+total_marks = int(input("Enter total marks: "))
+result = (ob_marks / total_marks) * 100
+print(result)
---
+++ Week 7/task 2.py
@@ -0,0 +1,17 @@
+ob_marks = int(input("Enter your marks: "))
+total_marks = int(input("Enter your total marks: "))
+result = (ob_marks / total_marks) * 100
+if result >= 90:
+    print("A+")
+elif result >= 85:
+    print("A-")
+elif result >= 80:
+    print("B+")
+elif result >= 75:
+    print("B-")
+elif result >= 70:
+    print("C+")
+elif result >= 65:
+    print("C-")
+elif result >= 60:
+    print("F")
